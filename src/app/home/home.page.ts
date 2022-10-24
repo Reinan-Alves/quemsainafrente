6128193832557d3d8d502d447eb92644b356edc2
unknown <reinanalves1988@gmail.com>
2022-10-24 19:00:25 -0300

    antes do deploy

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,113 @@
+import { Candidato } from './model/candidato';
+import { Component, OnInit } from '@angular/core';
+import { CandidatoService } from './service/candidato.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  public candidato: Candidato = {
+    nome: '',
+    votos: 0,
+  };
+  public listaDeCandidatos: Array<Candidato> = [];
+  public exibirNumero: number | undefined;
+  public totalDeVotos = 0;
+  public habilitado = false;
+  spinner = false;
+  constructor(private candidatoService: CandidatoService) {}
+
+  ngOnInit(): void {
+    this.spinner = true;
+    this.listarCandidatos();
+    setTimeout(() => {
+      this.spinner = false;
+      this.ordenarPorVotos();
+      this.enumerarPosicoes();
+      this.calcularTotalDeVotos();
+      this.calcularPercentualPorCandidato();
+    }, 1000);
+  }
+  public listarCandidatos() {
+    return this.candidatoService.listarCandidatos().subscribe({
+      next: (res) => (this.listaDeCandidatos = res),
+      error: (err) => console.log(err),
+    });
+  }
+  public alterarCandidato(candidato: Candidato) {
+    return (
+      this.candidatoService.atualizarCandidato(candidato).subscribe({
+        next: (res) => {
+          // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+          alert('Realizado com sucesso'), res;
+        },
+        error: (err) => console.log(err),
+      })
+    );
+  }
+  registrarVoto(valor: string) {
+    //localStorage.setItem('votou', 'false');
+    if (localStorage.getItem('votou') !== 'true') {
+      this.calcularTotalDeVotos();
+      this.habilitado = false;
+      let contador = 0;
+      const id = parseInt(valor, 10);
+      this.listaDeCandidatos.map((candidato) => {
+        // eslint-disable-next-line eqeqeq
+        if (id == candidato.id) {
+          contador = candidato.votos;
+          contador++;
+          candidato.votos = contador;
+          this.alterarCandidato(candidato);
+        }
+      });
+      localStorage.setItem('votou', 'true');
+      location.reload();
+    } else {
+      alert('Um voto por pessoa é o ideal para um resultado mais fiel');
+    }
+  }
+  calcularTotalDeVotos() {
+    let acumulador = 0;
+    this.listaDeCandidatos.map((c) => {
+      acumulador += Number(c.votos);
+    });
+    this.totalDeVotos = acumulador;
+    console.log(this.totalDeVotos);
+  }
+  calcularPercentualPorCandidato() {
+    let i = 0;
+    this.listaDeCandidatos.map((c) => {
+      const contador = Number((Number(c.votos) / this.totalDeVotos) * 100);
+      setTimeout(() => {
+        document.getElementsByClassName('tdPercentual')[i].innerHTML =
+          contador.toFixed(2).toString() + '%';
+        i++;
+      }, 0);
+    });
+  }
+  ordenarPorVotos() {
+    this.listaDeCandidatos = this.listaDeCandidatos.sort((c1, c2) => {
+      if (c1.votos < c2.votos) {
+        return 1;
+      } else if (c1.votos > c2.votos) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  enumerarPosicoes() {
+    let i = 0;
+    this.listaDeCandidatos.forEach((e, index) => {
+      const contador = index + 1;
+      setTimeout(() => {
+        document.getElementsByClassName('tdColocacao')[i].innerHTML =
+          contador.toString() + 'ª';
+        i++;
+      }, 0);
+    });
+  }
+}
---
+++ src/app/home/service/candidato.service.ts
@@ -0,0 +1,62 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Candidato } from '../model/candidato';
+@Injectable({
+  providedIn: 'root',
+})
+export class CandidatoService {
+  public emitEvent = new EventEmitter();
+
+  private url = 'https://candidato-api-fake.herokuapp.com/';
+
+  constructor(private http: HttpClient) {}
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  httpOptions = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    headers: new HttpHeaders({ 'Content-type': 'application/json' }),
+  };
+
+  public listarCandidatos(): Observable<Array<Candidato>> {
+    return this.http.get<Array<Candidato>>(`${this.url}candidato`).pipe(
+      (res) => res,
+      (error) => error
+    );
+  }
+  public cadastrarCandidatos(candidato: Candidato): Observable<Candidato> {
+    return this.http
+      .post<Candidato>(
+        `${this.url}cadidato`,
+        JSON.stringify(candidato),
+        this.httpOptions
+      )
+      .pipe(
+        (res) => res,
+        (error) => error
+      );
+  }
+  public removerCandidato(candidato: Candidato): Observable<Candidato> {
+    return this.http
+      .post<Candidato>(
+        `${this.url}cadidato/${candidato.id}`,
+        JSON.stringify(candidato),
+        this.httpOptions
+      )
+      .pipe(
+        (res) => res,
+        (error) => error
+      );
+  }
+  public atualizarCandidato(candidato: Candidato): Observable<Candidato> {
+    return this.http
+      .put<Candidato>(
+        `${this.url}candidato/${candidato.id}`,
+        JSON.stringify(candidato),this.httpOptions
+      )
+      .pipe(
+        (res) => res,
+        (error) => error
+      );
+  }
+}
